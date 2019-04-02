2e94772a3bf4a0711088dc9e4afbecb6bc96232c
pengkeng <936506220@qq.com>
2019-04-02 23:00:37 +0800

    新增网络框架

--- app/src/main/java/com/elevator/elevatormonitor/Retrofit/RetrofitManager.java
+++ app/src/main/java/com/elevator/elevatormonitor/Retrofit/RetrofitManager.java
@@ -1,7 +1,12 @@
 package com.elevator.elevatormonitor.Retrofit;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class RetrofitManager {
     private static RetrofitManager mInstance;
+    private static String baseUrl = "http://112.74.63.8:8080";
+
 
     public static RetrofitManager getInstance() {
         if (mInstance == null) {
@@ -15,8 +20,12 @@ public class RetrofitManager {
     }
 
 
-    private void getRetrofit(){
+    public Retrofit getRetrofit() {
+        Retrofit mRetrofit = new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
+        return mRetrofit;
     }
-
 }
